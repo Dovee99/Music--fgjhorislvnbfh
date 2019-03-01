7d2d5ec4e5cf20747e4d859b68c804f7c7aea657
Dovee99 <44682400+Dovee99@users.noreply.github.com>
2019-03-01 18:43:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,18 +1,20 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();// npm install discord.js --save
  
+lient.on("ready", () => {
 
-
-    
-     var guild = client.guilds.get('508627023808430130');
+     var guild = client.guilds.get('اي دي السيرفر');
          setInterval(function(){
-var role = guild.roles.find(role=> role.name === 'Support Team');
+var role = guild.roles.find(role=> role.name === 'اسم الرتبة');
 if(!role) return;
              role.edit({
                  color : "RANDOM"
              });
          }, 1700)
+});
 
+    
+    
     
  
  client.login(process.env.BOT_TOKEN);
