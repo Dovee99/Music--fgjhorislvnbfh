fb98f5c063efb87e09c3b508551c5366aed693f1
Dovee99 <44682400+Dovee99@users.noreply.github.com>
2019-03-02 17:59:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+console.log("Scrpit By Dream");
 
 
- client.on("ready", () => {
+client.on("ready", () => {
+let channel =     client.channels.get("541264198034915348")
+setInterval(function() {
+channel.send(`Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , `);
+}, 30)
+})
  
-     var guild = client.guilds.get('508627023808430130');//أيدي سيرفرك
-         setInterval(function(){
-var role = guild.roles.find(role=> role.name === 'VIP');//إسم الرتبة
-if(!role) return;
-             role.edit({
-                 color : "RANDOM"
-             });
-         }, 1700)
-});
-
-
-
     
     
  
