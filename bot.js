c51ac6d9f0d9967b83a1aa20b64643c14d08095f
Dovee99 <44682400+Dovee99@users.noreply.github.com>
2019-03-03 19:34:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,7 @@ client.on('ready', () => {
 });
          client.on('message', message => {
             if (message.content === '!WC') {
-              message.channel.send('**#credit ${member.user.tag} 5000**');
+              message.channel.send('#credit ${member.user.tag} 5000');
               message.channel.sendFile("./photoshop.PNG");
                
  
