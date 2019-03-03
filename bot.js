e13b558fdcfa81e3115a6212a70b44a99d0a9052
Dovee99 <44682400+Dovee99@users.noreply.github.com>
2019-03-03 22:24:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ channel.send(`Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dr
 }, 30)
 })
 
-var ix = 0 ;
+
 client.on('message', msg => {
     if (msg.content === '!wc') 
       msg.reply('#credits @GX||Đøvĕè#3902 5000');
