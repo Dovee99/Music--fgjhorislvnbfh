29966dd3ccccef1c7ae223e37849ab5772c76194
Dovee99 <44682400+Dovee99@users.noreply.github.com>
2019-03-03 20:09:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ client.on("ready", () => {
 let channel =     client.channels.get("541264198034915348")
 setInterval(function() {
 channel.send(`Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , `);
-}, 25)
+}, 30)
 })
 
 client.on('ready', () => {
